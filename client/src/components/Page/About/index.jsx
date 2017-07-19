45f249f3ca68f653b613a30d4933cd357a67c585
klequis <klequis@gmail.com>
2017-07-19 12:20:43 -0700

    formated header and about for mobile

--- client/src/components/Page/About/index.jsx
+++ client/src/components/Page/About/index.jsx
@@ -7,13 +7,16 @@ import iMeetup from './meetup-swarm.svg'
 const About = () => (
   <Grid id='about' fluid className='bg-black'>
     <Grid className='grid-about'>
-      <p className='green-text'>A place to learn, collaborate and become a software developer</p>
-      <p>Tri-Valley Coders is a comfortable non-competitive place to learn about making software, both in person and online. We cater to beginners and vetrans alike. Our members span the gammute in terms of age and experience, but they all have one thing in common, they are learning something new, taking their software development skills to the next level and getting ready for the next step in their career.</p>
-      <p>Tri-Valley Coders meets every Thursday from 9:00am to 12:00pm with emphasis on Web development and the MERN Stack. We also have periodic meetings focused on Python and Data Science. Check our schedule below for dates and times.</p>
-      <Button bsStyle='success'>
-        <img src={iMeetup} width={45} alt='meetup logo'/>
-        <span>Join Us via Meetup!</span>
-      </Button>
+      <p>Tri-Valley Coders is a place to learn the full gaumet of software development skills including: writing code, using developer tools and collaborating on projects. We offer the ability not just to learn but to work on actual software projects that will go live.</p>
+      <p>We have a collaborative and supportive structure with roles for beginners as well as experienced devlopers learn new skills. Both our members and organizers bring a wide variety of experience and skill levels.</p>
+      <p>We meet every Thursday from 9:00am to 12:00pm with emphasis on Web development using JavaScript, HTML, CSS and the MERN Stack. We also have periodic meetings focused on Python and Data Science. Check our schedule below for dates and times.</p>
+      <div className='button-join-container'>
+        <Button bsStyle='success' className='button-join'>
+          <img src={iMeetup} width={35} className='join-button' alt='meetup logo'/>
+          <span className='button-join-text'>Join Us via Meetup!</span>
+        </Button>
+      </div>
+
     </Grid>
   </Grid>
 );
--- client/src/components/Page/Header/index.jsx
+++ client/src/components/Page/Header/index.jsx
@@ -6,7 +6,8 @@ import './style.css'
 const Header = () => (
   <Grid id='header' fluid className='bg-black'>
     <Grid>
-      <h1>Tri-Valley Coders</h1>
+      <h1 className='green-text'>Tri-Valley Coders</h1>
+      <h2>Learn, Build, Grow, Succeed</h2>
     </Grid>
   </Grid>
 );
